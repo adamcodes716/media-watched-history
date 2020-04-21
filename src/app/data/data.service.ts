05969014510aaaeb00de1713f1f67351a3ef041c
adamcodes716 <adamcodes716@gmail.com>
2020-04-21 09:04:32 -0400

    Using Deb's Method

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -16,23 +16,24 @@ export class DataService {
 
   public getEmployees(): Observable<Employee[]>
   {
-    return this.http.get<Employee[]>(this.apiUrl);
+    // return this.http.get<Employee[]>(this.apiUrl);
+    return this.http.get<Employee[]>(this.apiUrl).pipe(
+      tap(data => console.log('All:  ' + JSON.stringify(data))),
+      catchError (this.handleError)
+    );
+    }
+
+
+
+ private handleError(err: HttpErrorResponse) {
+  let errorMessage = '';
+  if (err.error instanceof ErrorEvent) {
+    errorMessage = `An error occurred: ${err.error.message}`;
+  } else {
+    errorMessage = `Server returned code ${err.status}, error message is ${err.message}`;
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
+ }
 }
-
-
-// private handleError(err: HttpErrorResponse) {
-//  let errorMessage = '';
-//  if (err.error instanceof ErrorEvent) {
-//    errorMessage = `An error occurred: ${err.error.message}`;
-//  } else {
-//    errorMessage = `Server returned code ${err.status}, error message is ${err.message}`;
-//    console.error(errorMessage);
-//    return throwError(errorMessage);
-//  }
-// }
-
- // getSubscriptionTypes(): Observable<string>[]{
-    // return of(['Monthly', 'Annual', 'LifeTime']);
- // }
 
